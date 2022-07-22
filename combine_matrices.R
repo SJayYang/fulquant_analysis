8863fa4b9d0cd74b38b149ec8d12441ae0d02de9
SJayYang <Stanley.j.yang@gmail.com>
2022-07-22 22:06:17 +0000

    Added DESeq2 analysis

--- combine_matrices.R
+++ combine_matrices.R
@@ -1,7 +1,8 @@
-library(dplyr)
+suppressMessages(library(dplyr))
+suppressMessages(library(DESeq2))
 args = commandArgs(trailingOnly=TRUE)
 folder = args[1]
-folder <- "/Users/Yangs113/rda_files"
+folder <- "/home/yangs/full_ONT_data/ONT/NGS4416/RDA_files"
 file_names <- list.files(folder)
 
 matrices <- lapply(file_names, function(file) {
@@ -11,4 +12,17 @@ matrices <- lapply(file_names, function(file) {
 
 # combined <- merge(matrices[[1]], matrices[[2]], by = 'row.names', all = 'true')
 combined2 <- do.call(merge, c(matrices, by = 'row.names', all = 'true'))
-saveRDS(combined2, file = file.path(folder, "combined_matrix.rds"))
+combined3 <- combined2[,-1]
+rownames(combined3) <- combined2[,1]
+saveRDS(combined3, file = file.path(folder, "combined_matrix.rds"))
+
+cts <- readRDS(file = file.path(folder, "combined_matrix.rds"))
+
+colData <- readRDS(file = file.path(folder, "annotColData.rds"))
+cts <- na.omit(cts)
+
+dds <- DESeqDataSetFromMatrix(countData = cts, colData = colData, design = ~ condition)
+
+dds <- DESeq(dds)
+res <- results(dds)
+res
