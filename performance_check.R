d78b8e1fe236242ea305a1c3959162616efbb7c5
SJayYang <Stanley.j.yang@gmail.com>
2022-08-01 21:46:40 +0000

    Changed name of output file

--- performance_check.R
+++ performance_check.R
@@ -1,5 +1,8 @@
-gtf.file = "/home/yangs/FulQuant/hg38.98_ERCC_SIRV/SIRV3_ERCC_Homo_sapiens.GRCh38.98.gtf"
+
+# This script writes the transcript names from the reference genome
+folder = "/home/yangs/FulQuant/hg38.98_ERCC_SIRV"
+gtf.file = file.path(folder, "SIRV3_ERCC_Homo_sapiens.GRCh38.98.gtf")
 gtf.gr = rtracklayer::import(gtf.file) # creates a GRanges object
 gtf.df = as.data.frame(gtf.gr)
 genes = unique(gtf.df[ ,"transcript_name"])
-write.table(genes, file="gene_ID.gene_name.txt", row.names = FALSE, quote=FALSE, col.names = FALSE)
+write.table(genes, file=file.path(folder, "transcript_name_list.txt"), row.names = FALSE, quote=FALSE, col.names = FALSE)
