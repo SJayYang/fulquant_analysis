7d8939d50ce1992063673450c03a38b2fdd8c091
SJayYang <Stanley.j.yang@gmail.com>
2022-08-01 21:47:13 +0000

    Print out number of transcripts for sensitivity and precision analysis

--- performance_check.sh
+++ performance_check.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
+# Run this before
 # Rscript performance_check.R
 genome_folder=/home/yangs/FulQuant/hg38.98_ERCC_SIRV
-gene_list=$genome_folder/gene_ID.gene_name.txt
-output_folder=/home/yangs/ONT_comparisons/Dopa_Motor_m/combined/tx_annot
+genome_transcript_list=$genome_folder/transcript_name_list.txt
+comparison=$1
+output_folder=/home/yangs/ONT_comparisons/${comparison}/combined/tx_annot
 bed_file=$output_folder/tx_human_final.bed.gz
-zcat $bed_file | cut -f4 | sort -u > $output_folder/gene_list.txt
-sort -u $gene_list > $genome_folder/sorted_transcriptID.txt
-join $output_folder/gene_list.txt $genome_folder/sorted_transcriptID.txt > $output_folder/combined.txt
+detectedGenesList=$output_folder/detectedTranscript.txt
+zcat $bed_file | cut -f4 | sort -u > $detectedGenesList
+sort -u $genome_transcript_list > $genome_folder/sorted_transcriptID.txt
+join $detectedGenesList $genome_folder/sorted_transcriptID.txt > $output_folder/combined.txt
+numTranscriptsSharedWRef=$(wc -l < ${output_folder}/combined.txt | xargs)
+echo "TP: Number of isoforms detected from reference: ${numTranscriptsSharedWRef}"
+numTranscriptInRef=$(wc -l < $genome_transcript_list | xargs)
+echo "TP + FN: Number of isoforms in reference: ${numTranscriptInRef} "
+numTranscriptDetected=$(wc -l < $detectedGenesList | xargs)
+echo "TP + FP: Number of isoforms detected total ${numTranscriptDetected}"
