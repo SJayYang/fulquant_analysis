bbd2108c5c0f0ca48be915b5dbbbbdba27140f21
Stanley Yang <stanley.j.yang@gmail.com>
2022-08-02 02:05:39 +0000

    Scripts to replicate transcript num analysis

---
+++ gtf_read_lengths.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# filename: aaa.py
+# python3
+
+import os
+import sys
+import gzip
+
+gtf_file = 'out.gtf'
+
+d = {}
+with open(gtf_file, 'rt') as r:
+    for line in r:
+        if line.startswith('#'):
+            continue
+        fields = line.strip().split('\t')
+        if not fields[2] == 'transcript':
+            continue
+        gene = tx = ''
+        for spec in fields[8].split(';'):
+            if spec.startswith('gene_id'):
+                gene = spec.replace('gene_id ', '')
+            elif spec.startswith('transcript_id'):
+                tx = spec.replace('transcript_id ', '')
+            else:
+                continue
+        d[gene] = d.get(gene, 0) + 1
+
+for k, v in d.items():
+   print(k, v)
---
+++ lengths.sh
@@ -0,0 +1,4 @@
+genome_folder=/home/yangs/fulquant_test_data/combined/tx_annot
+genome_file=$genome_folder/out.gtf
+python gtf_read_lengths.py $genome_file > $genome_folder/lengths.txt
+# cut -f1 -d "-" $genome_folder/lengths.txt | tr -d '"' | uniq -c | sed -e 's/^[[:space:]]*//' | cut -f1 -d " " > lengths_num.txt
