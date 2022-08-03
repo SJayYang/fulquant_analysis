7e5a0a6a2b75d1983eb7e27b008fe762d7aeb41a
Stanley Yang <stanley.j.yang@gmail.com>
2022-08-03 23:37:16 +0000

    Added code to find number of genes detected

--- get_num_clusters_isoforms.R
+++ get_num_clusters_isoforms.R
@@ -10,3 +10,17 @@ for (i in 1:length(exp_names)) {
 ### Get num of isoforms after filtering
 load('clusters_quant.rda')
 colSums(runCountMat != 0)
+
+# Get num of genes 
+runCount <- readRDS("clusters_quant_runCount.rds")
+just_1 <- as.data.frame(runCount[, 1])
+colnames(just_1) <- "expression"
+just1_nonzero <- subset(just_1, just_1$expression != 0)
+cl_names <- unique(rownames(just1_nonzero))
+
+
+load('tx_human.rda')
+gene_names <- mclapply(cl_names, function(cl) {
+	subset(finalTx, grepl(cl, finalTx$all_cl))$associated_gene
+})
+length(unique(unlist(gene_names)))
