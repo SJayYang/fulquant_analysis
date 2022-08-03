870a9553c22685065fee454cd965c6294869161a
SJayYang <stanley.j.yang@gmail.com>
2022-08-02 18:12:48 -0700

    Replicate figure 1C

--- replicateFig1.R
+++ replicateFig1.R
@@ -27,3 +27,20 @@ data <- melt(df)
 
 ggplot(data, aes(x=value, fill=variable)) +
   geom_density(alpha=.25) + scale_x_continuous(trans='log2', limits = c(64, 16384))
+
+# Repliace number of isoforms per gene 
+genome_isoforms="~/isoform_num_genome.txt"
+transcripts_isoforms="~/isoform_num_transcripts.txt"
+sequins_isoforms="~/isoform_num_sequins.txt"
+genome_isoforms_table <- read.table(genome_isoforms)
+transcripts_isoforms_table <- read.table(transcripts_isoforms)
+sequins_isoforms_table <- read.table(sequins_isoforms)
+genome_isoforms_vector <- genome_isoforms_table$V2
+sequins_isoforms_vector <- sequins_isoforms_table$V2
+transcripts_isoforms_vector <- transcripts_isoforms_table$V1
+length(sequins_isoforms_vector) <- length(genome_isoforms_vector)
+length(transcripts_isoforms_vector) <- length(genome_isoforms_vector)
+df <- data.frame(genome = genome_isoforms_vector, sequins = sequins_isoforms_vector, transcripts = transcripts_isoforms_vector)
+data <- melt(df)
+ggplot(data, aes(x=value, fill=variable)) +
+  geom_density(alpha=.15) + scale_x_continuous(trans='log2', limits = c(1, 32)) 
