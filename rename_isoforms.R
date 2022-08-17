b7d9def6485de3f3d431bfabbb9e78dcbabae5d6
SJayYang <stanley.j.yang@gmail.com>
2022-08-17 10:36:05 -0700

    Put code into functions for cleaner use

--- rename_isoforms.R
+++ rename_isoforms.R
@@ -1,18 +1,23 @@
+rename_matrix <- function(matrix) {
+	load('~/FulQuant/genome/tx.rda')
+	# transcripts <- rownames(runCountMat)
+	# SIRV_names <- transcripts[grepl("SIRVomeERCCome", transcripts)]
+	tx$clname <- gsub("chr", "", tx$clname)
+	tx$clname <- gsub("SIRV", "SIRVomeERCCome", tx$clname)
+	filtered <- tx[tx$clname %in% rownames(matrix)]
+	# filtered <- tx[tx$clname %in% SIRV_names]
+	# filtered <- tx[grepl("SIRV", tx$transcript_id)]
+	filtered <- filtered[, c('clname', 'gene_id', 'gene_name', 'transcript_id', 'transcript_name')]
+	matched <- match(rownames(matrix), filtered$clname)
+	gr <- as.data.frame(matrix)
+	gr$transcript_name <- filtered$transcript_name[matched]
+	gr$gene_id <- filtered$gene_id[matched]
+	return(gr)
+}
+
 # Rename the entire countMatrix
 load('clusters_quant.rda')
-load('~/FulQuant/genome/tx.rda')
-transcripts <- rownames(runCountMat)
-SIRV_names <- transcripts[grepl("SIRVomeERCCome", transcripts)]
-tx$clname <- gsub("chr", "", tx$clname)
-tx$clname <- gsub("SIRV", "SIRVomeERCCome", tx$clname)
-filtered <- tx[tx$clname %in% rownames(runCountMat)]
-# filtered <- tx[tx$clname %in% SIRV_names]
-# filtered <- tx[grepl("SIRV", tx$transcript_id)]
-filtered <- filtered[, c('clname', 'gene_id', 'gene_name', 'transcript_id', 'transcript_name')]
-matched <- match(rownames(runCountMat), filtered$clname)
-gr <- as.data.frame(runCountMat)
-gr$transcript_name <- filtered$transcript_name[matched]
-gr$gene_id <- filtered$gene_id[matched]
+gr <- rename_matrix(runCountMat)
 
 NA_vals <- gr[is.na(gr$transcript_name), ]
 has_transcript <- gr[!is.na(gr$transcript_name), ]
@@ -21,12 +26,5 @@ write.table(gr, file = "countMatrixNames.txt", sep = "\t")
 
 # Rename the files detected through DESEQ2
 tables <- readRDS('DASAnalysisResults.rds')
-load('~/FulQuant/genome/tx.rda')
-tx$clname <- gsub("chr", "", tx$clname)
-tx$clname <- gsub("SIRV", "SIRVomeERCCome", tx$clname)
-filtered <- tx[tx$clname %in% rownames(tables)]
-filtered <- filtered[, c('clname', 'gene_id', 'gene_name', 'transcript_id', 'transcript_name')]
-matched <- match(rownames(tables), filtered$clname)
-tables$transcript_name <- filtered$transcript_name[matched]
-tables$gene_id <- filtered$gene_id[matched]
+rename_matrix(tables)
 saveRDS(tables, "DASAnalysisResults_named.rds")
