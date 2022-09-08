1d740d8494848888dcab6f7ca6cc1c56e060d3c1
SJayYang <Stanley.j.yang@gmail.com>
2022-09-08 04:50:10 +0000

    made the sensitivity and precision sample specific

--- matched_cluster.R
+++ matched_cluster.R
@@ -13,20 +13,22 @@ merge_tables <- function(mat, save_file) {
 	countMatrix_df = as.data.frame(mat)
 	countMatrix_df$clname = rownames(mat)
 	mergedCountRefMatrix_df <- merge(refTranscripts_df, countMatrix_df, by = "clname", all = TRUE)
-	print("True positive: Matched cluster and values are existent")
-	tp_entries <- mergedCountRefMatrix_df[!is.na(mergedCountRefMatrix_df$nannot) & !is.na(mergedCountRefMatrix_df[[39]]), ]
-	tp_val <- dim(tp_entries[tp_entries[[39]] > 0,])[1]
-	full_vals <- dim(refTranscripts_df)[1]
-	print(folder)
-	print("sensitivity")
-	print(tp_val)
-	print("total")
-	print(full_vals)
-	fp_val <- dim(mergedCountRefMatrix_df[!is.na(mergedCountRefMatrix_df[[39]]) & is.na(mergedCountRefMatrix_df$nannot), ])[1]
-	print("precision")
-	print(tp_val)
-	print(tp_val + fp_val)
 
+	num_samples <- length(colnames(countMatrix_df)) - 1 
+	print(folder)
+	lapply(seq(1:num_samples), function(x) {
+		tp_entries <- mergedCountRefMatrix_df[!is.na(mergedCountRefMatrix_df$nannot) & !is.na(mergedCountRefMatrix_df[[39]]), ]
+		tp_val <- dim(tp_entries[tp_entries[[33 + x]] > 0,])[1]
+		full_vals <- dim(refTranscripts_df)[1]
+		print(colnames(countMatrix_df)[x])
+		print("sensitivity")
+		print(tp_val / full_vals)
+
+		fp_val <- dim(mergedCountRefMatrix_df[!is.na(mergedCountRefMatrix_df[[33 + x]]) & is.na(mergedCountRefMatrix_df$nannot), ])[1]
+		print("precision")
+		print(tp_val / (tp_val + fp_val))
+		})
+ 
 	save(countMatrix_df, refTranscripts_df, mergedCountRefMatrix_df, file = save_file)
 	write.table(mergedCountRefMatrix_df, file.path(folder, 'combined_annot.csv'), sep = '\t')
 }
